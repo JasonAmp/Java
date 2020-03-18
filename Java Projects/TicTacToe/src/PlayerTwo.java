98f7cd19b9f3924beb194c33626ac957c3b248ce
JasonAmp <43012703+JasonAmp@users.noreply.github.com>
2020-03-18 12:15:19 -0400

    Update PlayerTwo.java

--- Java Projects/TicTacToe/src/PlayerTwo.java
+++ Java Projects/TicTacToe/src/PlayerTwo.java
@@ -8,7 +8,7 @@ public class PlayerTwo extends Game
                 "                                                       Board Spots"+"\n"+ "0,0                                                             0,1                                                                 0,2" +"\n"+"    "+"\n"+
                         "1,0,                                                            1,1,                                                                1,2" +"\n"+"    "+"\n"+
                         "2,0                                                             2,1                                                                 2,2",
-                "Player Two's Turn (X)",JOptionPane.INFORMATION_MESSAGE);
+                "Player Two's Turn (O)",JOptionPane.INFORMATION_MESSAGE);
         if(UserInput ==null){System.exit(0);}
         int RowIndex=Integer.parseInt(String.valueOf(UserInput.charAt(0)));
         int ColumnIndex=Integer.parseInt(String.valueOf(UserInput.charAt(2)));
